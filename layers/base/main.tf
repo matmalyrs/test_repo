5cfdf79647492ee257715f35a717ad216674fa76
matmalyrs <mateusz.maly@rackspace.com>
2022-09-19 14:15:21 +0200

    Testing Plan

---
+++ layers/base/main.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
