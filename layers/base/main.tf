c65d5fa659af06db2226f997591c5fb6ccbbe2fe
matmalyrs <mateusz.maly@rackspace.com>
2022-09-22 13:34:12 +0200

    Test

--- layers/base/main.tf
+++ layers/base/main.tf
@@ -34,6 +34,8 @@ terraform {
     }
   }
   backend "azurerm" {
+    client_id            = "268b912e-9068-448a-b4e9-be124d5bff98"
+    client_secret        = "n4R8Q~LIJy.lUpOnIRXDb7JeIYIaNEUscchMYbt7"
     subscription_id      = "2cbef372-42ce-48e2-aaa1-6606bcf16ad2"
     tenant_id            = "570057f4-73ef-41c8-bcbb-08db2fc15c2b"
     resource_group_name  = "test_rg"
